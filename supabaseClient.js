323f0aa391365e537129d49a8a7a7064dfdc0518
Naturverse1 <turianmediacompany@gmail.com>
2025-08-04 07:08:55 +0700

    chore: update Supabase client credentials

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://gxewpstvuoofdqanhjzi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ1Zl6Imd4ZXdwc3R2dW9vZmRxYW5oanppliwicm9sZSI6InFub24iLCJpYXQiOjE3NTQwNjM1NzUsImV4cCI6MjA2OTYzOTU3NX0._W8NSjDmv4dXoE701SKMQ60J8D3DTCCcRX5WRJYr_E'
+const supabaseAnonKey = 'sb_publishable_BDdV5w6oEVsrnwBL5f-zhw_Xdjkm7ip'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
