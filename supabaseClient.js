62cdccfff9d0adf91978ba789caa58fdd83a7ede
Naturverse1 <turianmediacompany@gmail.com>
2025-08-04 07:26:50 +0700

    feat: add Supabase test button

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.53.0/+esm'
+
 const supabaseUrl = 'https://gxewpstvuoofdqanhjzi.supabase.co'
 const supabaseAnonKey = 'sb_publishable_BDdV5w6oEVsrnwBL5f-zhw_Xdjkm7ip'
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
